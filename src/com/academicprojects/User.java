3d76237192341a4b47c73899e4a5ee0b5658dc48
Izabela Kułakowska <izabel.kulak@gmail.com>
2014-04-29 01:37:15 +0200

    First ver - too much libraries and without answers

---
+++ src/com/academicprojects/User.java
@@ -0,0 +1,29 @@
+package com.academicprojects;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class User {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public Map<String, String> getInfo() {
+		return info;
+	}
+	public void setInfo(Map<String, String> info) {
+		this.info = info;
+	}
+	private String ip;
+	private Map<String, String> info = new HashMap<String,String>();
+
+}
