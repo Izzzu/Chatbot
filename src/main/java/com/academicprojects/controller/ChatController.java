12f5c0ccfa92d192ddd4c5a67633512e4e37240d
izabela <izabel.kulak@gmail.com>
2015-02-03 22:20:07 +0100

    scope-session

--- src/main/java/com/academicprojects/controller/ChatController.java
+++ src/main/java/com/academicprojects/controller/ChatController.java
@@ -4,6 +4,7 @@ import com.academicprojects.model.Answer;
 import com.academicprojects.model.Chatbot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Scope("session")
 @Controller
 public class ChatController {
 
