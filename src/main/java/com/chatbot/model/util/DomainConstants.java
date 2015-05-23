0279920bb99f8c07349d88b89da6d79d15525149
izabela <izabel.kulak@gmail.com>
2015-05-23 21:36:31 +0200

    add kto to opening questions

--- src/main/java/com/chatbot/model/util/DomainConstants.java
+++ src/main/java/com/chatbot/model/util/DomainConstants.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 
 public class DomainConstants {
-    public static final List<String> OPENINGS_OF_QUESTION = Arrays.asList("czy", "czym", "dlaczego", "kiedy", "jak", "jaka", "jaki", "jakie", "jakiego", "jakiej", "po", "czym", "co", "o");
+    public static final List<String> OPENINGS_OF_QUESTION = Arrays.asList("czy", "czym", "dlaczego", "kiedy", "jak", "jaka", "jaki", "jakie", "jakiego", "jakiej", "po", "czym", "co", "o", "kto");
 
 }
